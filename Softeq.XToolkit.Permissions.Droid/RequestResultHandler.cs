30196fbfb4ffcfa0a330f1b02f19a5cc1aa1382e
vadim pylsky <vadim.pylsky@softeq.com>
2018-10-18 15:00:41 +0300

    permissions

---
+++ Softeq.XToolkit.Permissions.Droid/RequestResultHandler.cs
@@ -0,0 +1,16 @@
+﻿// Developed by Softeq Development Corporation
+// http://www.softeq.com
+
+using Android.Runtime;
+using Plugin.Permissions;
+
+namespace Softeq.XToolkit.Permissions.Droid
+{
+    public static class RequestResultHandler
+    {
+        public static void Handle(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            PermissionsImplementation.Current.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
---
+++ Softeq.XToolkit.Permissions/IPermissionsManager.cs
@@ -0,0 +1,15 @@
+﻿// Developed by Softeq Development Corporation
+// http://www.softeq.com
+
+using System.Threading.Tasks;
+
+namespace Softeq.XToolkit.Permissions
+{
+    public interface IPermissionsManager
+    {
+        Task<PermissionStatus> CheckWithRequestAsync(Permission permission);
+        Task<PermissionStatus> CheckAsync(Permission permission);
+        void OpenSettings();
+    }
+
+}
