61f18534a503223f65e610f6a16234da22a13219
Yauheni Pakala <evgeniy.pakalo@gmail.com>
2019-03-05 18:39:50 +0300

    Add extensions for TextView & UIView

---
+++ Softeq.XToolkit.Common.Droid/Extensions/TextViewExtentions.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using Android.Graphics;
+using Android.Text;
+using Android.Text.Style;
+using Android.Widget;
+
+namespace Softeq.XToolkit.Common.Droid.Extensions
+{
+    public static class TextViewExtentions
+    {
+        public static void HighlightStrings(this TextView textView,
+            IEnumerable<(int Start, int Length)> ranges,
+            Color color)
+        {
+            var text = textView.Text;
+            var spannedString = new SpannableString(text);
+
+            foreach (var (Start, Length) in ranges)
+            {
+                spannedString.SetSpan(
+                        new ForegroundColorSpan(color),
+                        Start,
+                        Start + Length,
+                        SpanTypes.InclusiveInclusive);
+            }
+
+            textView.SetText(spannedString, TextView.BufferType.Spannable);
+        }
+    }
+}
