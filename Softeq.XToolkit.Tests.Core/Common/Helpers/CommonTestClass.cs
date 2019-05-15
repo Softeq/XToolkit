aace5098c36be96e601b85a09abc5eab3ed3fef7
Siarhei Leushunou <siarhei.leushunou@softeq.com>
2019-05-15 16:56:07 +0300

    repo refactor

--- Softeq.XToolkit.Tests.Core/Common/Helpers/CommonTestClass.cs
+++
@@ -1,9 +0,0 @@
-﻿// Developed by Softeq Development Corporation
-// http://www.softeq.com
-
-namespace Softeq.XToolkit.Tests.Core.Common.Helpers
-{
-    public class CommonTestClass
-    {
-    }
-}
